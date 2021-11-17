d5d68517d21c2ae1f3835d7b2988c330ab85168b
abrosimov94vladislav@gmail.com <abrosimov94vladislav@gmail.com>
2021-11-17 11:55:09 +0300

    4-lesson: steps completed

--- 4-steps/src/App.js
+++ 4-steps/src/App.js
@@ -5,36 +5,68 @@ import React from "react";
 
 function App() {
 
-    const [records, setRecords] = React.useState([{id:1, date:"12.12.2021",distance:"4.2"},{id:2, date:"12.12.2021",distance:"4.2"}]);
-    const [currentDate, setCurrentDate] = React.useState('');
-    const [currentDistance, setCurrentDistance] = React.useState('');
+    const [records, setRecords] = React.useState([]);
+    const [lastId, setLastId] = React.useState(0);
+    const [form, setForm] = React.useState({date: '', distance: 0})
 
     const onDeleteClick = (evt) => {
         const id = evt.target.name;
-        let arr = records.filter(rec => rec.id!=id);
+        let arr = records.filter(rec => rec.id != id);
         console.log(arr)
         setRecords(arr);
     }
 
-    const handleSubmit = (evt) =>{
+    const handleSubmit = (evt) => {
         evt.preventDefault();
+
+        let arr = records;
+        const currentDistance = parseFloat(form.distance);
+        if (form.date !== '' && currentDistance !== 0 && currentDistance.toString() !== "NaN") {
+
+            const recordsByDate = records.filter(rec => rec.date === form.date);
+            if (recordsByDate.length > 0) {
+                let recordByCurrentDate = recordsByDate[0];
+                let resultDistance = recordByCurrentDate.distance + currentDistance;
+                recordByCurrentDate = {
+                    id: recordByCurrentDate.id,
+                    date: recordByCurrentDate.date,
+                    distance: resultDistance
+                };
+                arr = arr.filter(rec => rec.id !== recordByCurrentDate.id);
+                arr.push(recordByCurrentDate);
+            } else {
+                const currentId = lastId + 1;
+                setLastId(currentId);
+                arr.push({id: currentId, date: form.date, distance: currentDistance})
+            }
+            setRecords(sortByDate(arr));
+        }
     }
 
-    const handleDateChange = () =>{
+    const sortByDate = (arr) => {
+        arr.sort(function (a, b) {
+            var dateA = new Date(a.date), dateB = new Date(b.date)
+            return dateB - dateA
+        })
+        return arr;
+    }
 
+    const handleDateChange = (evt) => {
+        setForm(prevForm => ({...prevForm, date: evt.target.value}));
     }
 
 
-    const handleDistanceChange = (evt) =>{
-        console.log(evt.target.value)
+    const handleDistanceChange = (evt) => {
+        setForm(prevForm => ({...prevForm, distance: evt.target.value}));
     }
 
-  return (
-      <div className="container">
-          <FormSteps handleSubmit={handleSubmit} onDateChange={handleDateChange} onDistanceChange={handleDistanceChange}/>
-          <TableSteps records={records} onDeleteClick={onDeleteClick}/>
-      </div>
-  )
+    return (
+        <div className="container">
+            <FormSteps handleSubmit={handleSubmit} onDateChange={handleDateChange}
+                       onDistanceChange={handleDistanceChange} form={form}/>
+            <TableSteps records={records} onDeleteClick={onDeleteClick}/>
+        </div>
+    )
 }
 
 export default App;
--- 4-steps/src/components/FormSteps.jsx
+++ 4-steps/src/components/FormSteps.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 
-export default function FormSteps({handleSubmit, onDateChange, onDistanceChange}) {
-
-    let date = '';
-    let distance = '';
-
+export default function FormSteps({handleSubmit, onDateChange, onDistanceChange, form}) {
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <div className="form_container">
                     <div className="date_container">
                         <div className="date_text">Дата(ДД.ММ.ГГ)</div>
-                        <input className="date" type="date" value={date} onChange={onDateChange}/>
+                        <input className="date" type="date" value={form.date} onChange={onDateChange}/>
                     </div>
                     <div className="km_container">
                         <div className="km_text">Пройдено км</div>
-                        <input className="km" id="distance" name="distance" type="number" value={distance} onChange={onDistanceChange}/>
+                        <input className="km" id="distance" name="distance" value={form.distance}
+                               onChange={onDistanceChange}/>
                     </div>
                     <div className="accept_container">
                         <button type="submit">ОК</button>
