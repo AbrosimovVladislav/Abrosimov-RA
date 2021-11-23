cf5b9e406269c22a25ce815f0989447bad942cb5
abrosimov94vladislav@gmail.com <abrosimov94vladislav@gmail.com>
2021-11-23 16:39:22 +0300

    5-lesson: somshit

--- 5-cards/src/App.js
+++ 5-cards/src/App.js
@@ -18,7 +18,6 @@ function App() {
             <Card {...card1}
                   title="Special title treatment"
                   text="With supporting text below as a natural lead-in to additional content.">
-                <img src={pic} className="card-img-top" alt="..."/>
             </Card>
         </div>
     )
